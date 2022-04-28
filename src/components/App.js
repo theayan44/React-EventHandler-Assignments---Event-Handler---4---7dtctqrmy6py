ab26d55d53dc8fd252ea52680f727fc7c82d5660
theayan44 <88241719+theayan44@users.noreply.github.com>
2022-04-28 21:42:43 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  let numClicks = 0;
+  let singleClickTimer;
+  const handleClick = () => {
+    numClicks++;
+    if (numClicks === 1) {
+      singleClickTimer = setTimeout(() => {
+        numClicks = 0;
+        console.log("single click!");
+      }, 400);
+    } else if (numClicks === 2) {
+      clearTimeout(singleClickTimer);
+      numClicks = 0;
+      console.log("double click!");
+    }
+  };
 
-  const handleDoubleClick = (event) =>{
-  
-  }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        id="dblclick-btn"
+        onClick={(event) => {
+          handleClick(event);
+        }}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
